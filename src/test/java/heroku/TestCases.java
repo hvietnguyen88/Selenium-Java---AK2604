3e8b79175514ab014959f9691e00469dea54efd4
hvietnguyen88 <hviet.nguyen88@gmail.com>
2026-04-23 15:13:55 +0300

    Commit TestCases

---
+++ src/test/java/heroku/TestCases.java
@@ -0,0 +1,26 @@
+package heroku;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCases {
+    @Test
+    public void loginWithValidCredentials() {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--headless=new");
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure");
+        driver.quit();
+    }
+}
